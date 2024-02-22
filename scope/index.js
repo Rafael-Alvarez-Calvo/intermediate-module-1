e7a9362cdab09a73750031c43406cb4af56ba147
danielemilio.romero.contractor <danielemilio.romero.contractor@bbva.com>
2024-02-22 13:53:46 +0100

    feat(*): add intermedial module 1

--- scope/index.js
+++ scope/index.js
@@ -1,53 +1,23 @@
-/*
-console.clear();
-let x = 1;
-if (x === 1) {
-    let x = 2;
-    console.log(x); // 2
-}
-console.log(x); // 1
-var y = 1;
-if (y === 1) {
-    var y = 2;
-    console.log(y); // 2
-}
-console.log(y); // 2
+const sum = (a, b) => {
 
- */
-
-var count = 2;
-var total = 0;
-function calcfOutside(numberparam) {
-    debugger;
-    console.log('desde param CALCF',this.paramCalcThis)
-    this.total +=  this.count * numberparam;
-    debugger;
-    console.log('function' , this.total);
+    return a + b;
 }
 
 
-const calc = (number) => {
-    console.log('desde param calc This',this.paramCalcThis);
-    this.total = this.count * number;
-    console.log('arrow function',this.total);
-}
 
-function printMsg() {
-    this.paramCalcThis = 3;
-    let paramCalcLet = 2;
+const curry = (f) => {
 
-    function calcfInside() {
-        console.log('desde param calc Let from inside', paramCalcLet);
+    return (n1) => {
+        //return f(2,3);
+        return (n2) => {
+            debugger;
+            return f(n1, n2)
+        }
     }
-
-    /*
-    calc(2);
-    calcfOutside(2);
-
-     */
-
-    setInterval(calcfInside, 2000)
-    //calcF(total, 2);
 }
 
-printMsg();
+
+const c = curry(sum);
+const response = c(1)(2)
+console.log(response);
+
---
+++ spread-operator/index.js
@@ -0,0 +1,76 @@
+/**
+ * Example 1
+ * Podemos evitar hacer un push utilizando el operador sprint para combinar dos arrays
+ */
+
+/*
+const numbers = [1,2,3,4];
+const otherNumber = [5,6,7];
+
+const total = [
+    ...numbers,
+    ...otherNumber,
+];
+
+console.log(total);
+
+ */
+
+/**
+ * Example 2
+ * Podemos utilizar el operador sprint para combinar objetos
+ *
+ */
+/*
+const objectNumber = {
+    key1: 1,
+    key2: 2,
+};
+
+const otherObjectNumber = {
+    key3: 3,
+    key4: 4,
+};
+
+const total = {
+    ...objectNumber,
+    ...otherObjectNumber,
+};
+
+console.log(total);
+
+ */
+
+/**
+ * Example 3
+ * Podemos utilizar operador spread cuando no sabemos cuantos parámetros en concreto va a recibir nuestra función
+ */
+
+/*
+const myfunction = (...params) => {
+    console.log(params);
+};
+
+myfunction(1,'string', true);
+
+*/
+
+/**
+ * Example 4
+ * Para pasarle las propiedades al constructor no podemos hacerlo como array el operador spread nos ayuda a esto
+ *
+ */
+
+/*
+const paramsConstructor = [1970, 0, 1];  // 1 Jan 1970
+let myDay = new Date(...paramsConstructor);
+console.log(myDay);
+ */
+
+/**
+ * Ojo!! con utilizar
+ *
+ */
+let a = [1,2,3,[4,5]];
+let b = [...a];
+a[3].push(6);
