18ef7e76185369582afc6e7d481263391e87fbfc
Rafael <rafael.alvarez@hotmail.es>
2024-02-22 17:26:23 +0100

    Add -> closures excercise

--- scope/index.js
+++ scope/index.js
@@ -1,7 +1,7 @@
 // RESUELVE EJERCICIOS
 
 var mensaje = "¡Hola, mundo!";
-    
+
 function saludo() {
     var mensaje = "¡Hola, function scope!";
     let mensaje2 = "¡Hola, function scope!";
@@ -14,12 +14,44 @@ saludo();
 console.log(mensaje);
 
 // ¿Cuál será la salida al ejecutar este código? ¿Por qué?
-// Output: "¡Hola, mundo!" "¡Hola, function scope!" "¡Hola, mundo!", 
+// Output: "¡Hola, mundo!" "¡Hola, function scope!" "¡Hola, mundo!",
 
 // Ahora, intenta quitar la declaración var de la variable mensaje en la función saludo() y ejecútalo nuevamente. ¿Cómo cambia el resultado?
-// Output: "¡Hola, mundo!" "¡Hola, function scope!" "¡Hola, mundo!", 
+// Output: "¡Hola, mundo!" "¡Hola, function scope!" "¡Hola, mundo!",
 
 // lAS DOS FORMAS DAN EL MISMO OUTPUT PORQUE EN ESTE CASO NO CAMBIA EL SCOPE
+
+
+// ## Ejercicio 2: Closures
+
+// Piensa en cómo puedes usar closures para crear funciones que implementen números sucesivos. Por ejemplo:
+
+const sumaUno = function() {
+    let _marcador = 0;
+
+    function sumar(){
+        _marcador++;
+
+        function sumar2(){
+            _marcador++;
+            return console.log(_marcador);
+
+            function sumar3(){
+                _marcador++;
+                return console.log(_marcador);
+            }
+        }
+
+        return console.log(_marcador);
+    }
+
+    return sumar;
+};
+
+sumaUno()(); // el resultado sería 1
+sumaUno()()()(); // el resultado sería 3
+
+// Intenta implementar esta funcionalidad usando closures.
 
 
 var x = 0;
